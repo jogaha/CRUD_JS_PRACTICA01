421fb0001f202e4c58ce34f1dbcea25273302114
Joel Gaona <jogaha04@gmail.com>
2022-08-10 09:38:53 -0700

    clase02 completada

--- service/client-service.js
+++ service/client-service.js
@@ -1,40 +1,8 @@
-const crearNuevaLinea = (nombre, email) => {
-  const linea = document.createElement("tr");
-  const contenido = `
-    <td class="td" data-td>
-      ${nombre}
-    </td>
-    <td>${email}</td>
-    <td>
-      <ul class="table__button-control">
-        <li>
-          <a
-            href="../screens/editar_cliente.html"
-            class="simple-button simple-button--edit"
-          >
-            Editar
-          </a>
-        </li>
-        <li>
-          <button class="simple-button simple-button--delete" type="button">
-            Eliminar
-          </button>
-        </li>
-      </ul>
-    </td>
-  `;
-  linea.innerHTML = contenido;
-  return linea;
-};
-
-const table = document.querySelector("[data-table]");
-
 //abrir http (metodo,url)
 //CRUD (Create, Read, Update, Delete)
 //Metodos (POST,GET,PUT/PATCH,DELETE)
 
-const listaClientes = () => {
-  const promise = new Promise((resolve, reject) => {
+/* const promise = new Promise((resolve, reject) => {
     const http = new XMLHttpRequest();
     http.open("GET", "http://localhost:3000/perfil");
 
@@ -51,17 +19,14 @@ const listaClientes = () => {
       }
     };
   });
-  return promise;
-};
-listaClientes()
-  .then((data) => {
-    data.forEach((perfil) => {
-      const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-      table.appendChild(nuevaLinea);
-    });
-  })
-  .catch((error) => {
-    alert("ocurrio un error");
-  });
+  return promise; */
+
+//Fetch API
+const listaClientes = () =>
+  fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
 //Promesas permiten trabajar de manera asincrona(no se tiene que esperar algun resultado para poder seguir utilizando el programa)
+
+export const clientServices = {
+  listaClientes,
+};
